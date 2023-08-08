2ae5b235b1a3486da3f24b38affff9d6448bc8e6
90536 <90536@DESKTOP-FCHRJ42>
2023-08-08 17:10:17 +0300

    Add project files.

---
+++ Services/Catalog/CasgemMicroservice.Services.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using CasgemMicroservice.Services.Catalog.Dtos.CategoryDtos;
+using CasgemMicroservice.Shared.Dtos;
+
+namespace CasgemMicroservice.Services.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<Response<List<ResultCategoryDto>>> GetAllCategoryAsync();
+        Task<Response<ResultCategoryDto>> GetByIdCategoryAsync(string id);
+        Task<Response<NoContent>> CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task<Response<NoContent>> UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task<Response<NoContent>> DeleteCategoryAsync(string id);
+    }
+}
